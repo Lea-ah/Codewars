6c6bd878072fac206f9f39f6a51944bcf0d8da85
lea moesch <leamoesch@gmail.com>
2022-07-13 14:16:36 +1000

    boolean to string

---
+++ 8kyu/boolean_to_string.js
@@ -0,0 +1,17 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// My Answer :
+
+function booleanToString(b){
+  //your code here
+  let Str = b.toString()
+  return Str
+}
+
+// refactored:
+
+// function booleanToString(b){
+//   return b.toString();
+// }
